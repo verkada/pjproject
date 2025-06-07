7c26a963e8afd0822770dd98f4ee1bfcc4479300
Matthew MacGregor <matthew.macgregor@verkada.com>
2025-06-07 01:46:08 +0000

    fixup! fixup! fixup! fixup! fixup! INT-5197: agc: Correctly copy AGC processed samples to frame buffer

--- pjmedia/src/pjmedia/conference.c
+++ pjmedia/src/pjmedia/conference.c
@@ -1838,9 +1838,9 @@ static pj_status_t read_port( pjmedia_conf *conf,
                 if (si.agc_rx) {
                     int leftover = ProcessCaptureAudioS16(&conf->agc, (int16_t*) cport->rx_buf, cport->rx_buf_count / conf->channel_count);
                     unsigned int samples_processed = cport->rx_buf_count - leftover * conf->channel_count;
+                    PJ_LOG(2,(THIS_FILE, "Processed %d samples out of %d", samples_processed, cport->rx_buf_count));
                     cport->rx_buf_count -= samples_processed;
 
-                    PJ_LOG(2,(THIS_FILE, "Processed %d samples", samples_processed));
                     pjmedia_copy_samples(frame, cport->rx_buf, samples_processed);
                     if (cport->rx_buf_count) {
                         pjmedia_move_samples(cport->rx_buf, cport->rx_buf + samples_processed, cport->rx_buf_count);
